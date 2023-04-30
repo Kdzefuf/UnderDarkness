88ed874e75cb07868651b61a3c4644b245a8421a
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2023-04-30 23:48:25 +0500

    .

--- RogueLike/Enemy.cs
+++ RogueLike/Enemy.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using RogueSharp;
 
 namespace RogueLike
@@ -20,17 +19,12 @@ namespace RogueLike
 
         public void Update()
         {
-            _path._pathFinder.ShortestPath(_map.GetCell(X, Y),
-                _map.GetCell(_player.X, _player.Y)); ;
-            X = _path.FirstCell.X;
-            Y = _path.FirstCell.Y;
+
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             float multiplier = Sprite.Width;
-            spriteBatch.Draw(Sprite, new Vector2(X * multiplier, Y * multiplier), null,
-                Color.White, 0.0f, Vector2.One, 1f, SpriteEffects.None, LayerDepth.Figures);
         }
     }
 }
